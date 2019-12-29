fc4aca43aef3076ff85e9135ae6c6b0b6c1f9fe5
AkshadaRK <akshadakurhe@gmail.com>
2019-12-29 12:24:12 +0530

    upload database

---
+++ database/Dc.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace database
+{
+    public delegate void DBdelegate();
+    class Dc
+    {
+        public event DBdelegate Add;
+        public event DBdelegate Delete;
+        MySqlConnection CN;
+        MySqlCommand CMD;
+        public void insert()
+        {
+            
+            CN = new MySqlConnection(ConfigurationManager.ConnectionStrings["InfowayMySqlConStr"].ConnectionString);
+                Console.WriteLine("Enter Emp ID");
+                int empid = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Emp Name");
+                String empname = Console.ReadLine();
+                Console.WriteLine("enter city");
+                String empcity = Console.ReadLine();
+
+
+                CN.Open();
+
+                CMD = new MySqlCommand("insert into emp1 values(@empid,@empname,@empcity)", CN);
+                CMD.CommandType = CommandType.Text;
+                CMD.Parameters.AddWithValue("empid", empid);
+                CMD.Parameters.AddWithValue("empname", empname);
+                CMD.Parameters.AddWithValue("empcity", empcity);
+                int i = CMD.ExecuteNonQuery();
+           
+            Console.WriteLine("Commands executed! Total rows affected are " +i);
+                Console.WriteLine("Done!");
+            if (i == 1)
+            {
+                if (Add!=null)
+                {
+                    Add();
+                }
+            }
+            Console.ReadLine();
+                Console.Clear();
+                CN.Close();
+          
+            
+        }
+        public void view()
+        {
+            CN = new MySqlConnection(ConfigurationManager.
+                ConnectionStrings["InfowayMySqlConStr"].ConnectionString);
+          
+            CN.Open();
+
+            CMD = new MySqlCommand("select * from emp1 ", CN);
+            CMD.CommandType = CommandType.Text;
+            using (MySqlDataReader reader = CMD.ExecuteReader())
+            {
+                Console.WriteLine("Emp Id\t\t\t Name \t\t\t City\t");
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format
+                        ("{0} \t\t\t | {1} \t\t\t |{2}\t",
+                        reader[0], reader[1], reader[2]));
+                }
+            }
+
+            Console.WriteLine("Commands executed! Total rows affected are "
+                + CMD.ExecuteNonQuery());
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+            Console.Clear();
+            CN.Close();
+        }
+
+
+        public void sort()
+        {
+            CN = new MySqlConnection(ConfigurationManager.
+                ConnectionStrings["InfowayMySqlConStr"].ConnectionString);
+
+            CN.Open();
+
+            CMD = new MySqlCommand("select * from emp1 order by empname ", CN);
+            CMD.CommandType = CommandType.Text;
+            using (MySqlDataReader reader = CMD.ExecuteReader())
+            {
+                Console.WriteLine("Emp Id\t\t\t Name \t\t\t City\t");
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format
+                        ("{0} \t\t\t | {1} \t\t\t |{2}\t",
+                        reader[0], reader[1], reader[2]));
+                }
+            }
+
+            Console.WriteLine("Commands executed! Total rows affected are "
+                + CMD.ExecuteNonQuery());
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+            Console.Clear();
+            CN.Close();
+        }
+        public void search()
+        {
+            CN = new MySqlConnection
+                (ConfigurationManager.ConnectionStrings["InfowayMySqlConStr"].
+                ConnectionString);
+
+            CN.Open();
+            Console.WriteLine("Enter Emp ID");
+            int empid = int.Parse(Console.ReadLine());
+            CMD = new MySqlCommand("select * from emp1 where " +
+                "empid1=@empid", CN);
+            CMD.Parameters.AddWithValue("empid", empid);
+
+            CMD.CommandType = CommandType.Text;
+            using (MySqlDataReader reader = CMD.ExecuteReader())
+            {
+                Console.WriteLine("Emp Id\t\t\t Name \t\t\t City\t");
+                while (reader.Read())
+                {
+                    Console.WriteLine(String.Format("{0} \t\t\t | {1} \t\t\t |{2}\t",
+                        reader[0], reader[1], reader[2]));
+                }
+            }
+
+            Console.WriteLine("Commands executed! Total rows affected are " + CMD.ExecuteNonQuery());
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+            Console.Clear();
+            CN.Close();
+        }
+        public void delete()
+        {
+            CN = new MySqlConnection(ConfigurationManager.ConnectionStrings["InfowayMySqlConStr"].ConnectionString);
+
+            CN.Open();
+            Console.WriteLine("Enter Emp ID You Want To delete");
+            int empid = int.Parse(Console.ReadLine());
+            CMD = new MySqlCommand("Delete from emp1 where empid1=@empid", CN);
+            CMD.Parameters.AddWithValue("empid", empid);
+            int i = CMD.ExecuteNonQuery();
+            Console.WriteLine("Commands executed! Total rows affected are " +i);
+            Console.WriteLine("Done!");
+            if (i == 1)
+            {
+                if (Delete!= null)
+                {
+                    Delete();
+                }
+            }
+            Console.ReadLine();
+            Console.Clear();
+            CN.Close();
+        }
+    }
+}
---
+++ database/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dc d = new Dc();
+            while (true)
+            {
+                Console.WriteLine("1.Add 2.Search 3.delete 4.view 5.sort ");
+            int ch =int.Parse( Console.ReadLine());
+            
+           
+                switch (ch)
+                {
+                    case 1:
+                        d.Add += new DBdelegate(Addmsg);
+                        d.insert();
+                        break;
+                    case 3:
+                        d.Delete+= new DBdelegate(Deletemsg);
+                        d.delete();
+                        break;
+                    case 2:
+                        d.search();
+                        break;
+                   case 4:
+                        d.view();
+                        break;
+
+                    case 5:
+                        d.sort();
+                        break;
+                }
+
+            
+            }
+
+        
+
+        }
+        public static void Addmsg()
+        {
+            Console.WriteLine("New Record is added");
+     
+        }
+        public static void Deletemsg()
+        {
+            Console.WriteLine(" Record is deleted");
+
+
+        }
+    }
+}
